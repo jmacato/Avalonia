e872f15b2c723bab55958e089a27cae7102a0d4c
Steven Kirk <grokys@gmail.com>
2018-02-16 21:34:16 +0100

    Compare test results with Subject.

--- tests/Avalonia.Base.UnitTests/Reactive/LightweightObservableBaseTests.cs
+++ tests/Avalonia.Base.UnitTests/Reactive/LightweightObservableBaseTests.cs
@@ -9,10 +9,10 @@ namespace Avalonia.Base.UnitTests.Reactive
 {
     public class LightweightObservableBaseTests
     {
-        [Fact]
-        public void Subscriber_Is_Notified_Of_Value()
+        [Theory]
+        [MemberData(nameof(GetTargets))]
+        public void Subscriber_Is_Notified_Of_Value(ISubject<string> target)
         {
-            var target = new TestSubject();
             var result = new List<string>();
 
             target.Subscribe(
@@ -26,10 +26,10 @@ namespace Avalonia.Base.UnitTests.Reactive
             Assert.Equal(new[] { "foo", "bar" }, result);
         }
 
-        [Fact]
-        public void Subscriber_Is_Notified_Of_Completion()
+        [Theory]
+        [MemberData(nameof(GetTargets))]
+        public void Subscriber_Is_Notified_Of_Completion(ISubject<string> target)
         {
-            var target = new TestSubject();
             var result = new List<string>();
 
             target.Subscribe(
@@ -44,10 +44,10 @@ namespace Avalonia.Base.UnitTests.Reactive
             Assert.Equal(new[] { "foo", "bar", "completed" }, result);
         }
 
-        [Fact]
-        public void Subscriber_Is_Notified_Of_Error()
+        [Theory]
+        [MemberData(nameof(GetTargets))]
+        public void Subscriber_Is_Notified_Of_Error(ISubject<string> target)
         {
-            var target = new TestSubject();
             var result = new List<string>();
 
             target.Subscribe(
@@ -62,10 +62,10 @@ namespace Avalonia.Base.UnitTests.Reactive
             Assert.Equal(new[] { "foo", "bar", "error" }, result);
         }
 
-        [Fact]
-        public void Subscribing_After_Completion_Notifies_Completion()
+        [Theory]
+        [MemberData(nameof(GetTargets))]
+        public void Subscribing_After_Completion_Notifies_Completion(ISubject<string> target)
         {
-            var target = new TestSubject();
             var result = new List<string>();
 
             target.OnCompleted();
@@ -77,10 +77,10 @@ namespace Avalonia.Base.UnitTests.Reactive
             Assert.Equal(new[] { "completed" }, result);
         }
 
-        [Fact]
-        public void Subscribing_After_Error_Notifies_Error()
+        [Theory]
+        [MemberData(nameof(GetTargets))]
+        public void Subscribing_After_Error_Notifies_Error(ISubject<string> target)
         {
-            var target = new TestSubject();
             var result = new List<string>();
 
             target.OnError(new Exception("error"));
@@ -92,10 +92,10 @@ namespace Avalonia.Base.UnitTests.Reactive
             Assert.Equal(new[] { "error" }, result);
         }
 
-        [Fact]
-        public void Disposing_Subscription_Stops_Notifications()
+        [Theory]
+        [MemberData(nameof(GetTargets))]
+        public void Disposing_Subscription_Stops_Notifications(ISubject<string> target)
         {
-            var target = new TestSubject();
             var result = new List<string>();
 
             var subscription = target.Subscribe(
@@ -111,7 +111,35 @@ namespace Avalonia.Base.UnitTests.Reactive
             Assert.Equal(new[] { "foo" }, result);
         }
 
-        private class TestSubject : LightweightObservableBase<string>
+        [Theory]
+        [MemberData(nameof(GetTargets))]
+        public void Disposing_Later_Subscription_Stops_Notification_Of_Next_Value(ISubject<string> target)
+        {
+            var result = new List<string>();
+            IDisposable subscription2 = null;
+
+            var subscription1 = target.Subscribe(
+                x => { result.Add(x); subscription2.Dispose(); },
+                ex => result.Add(ex.Message),
+                () => result.Add("completed"));
+            subscription2 = target.Subscribe(
+                x => result.Add(x),
+                ex => result.Add(ex.Message),
+                () => result.Add("completed"));
+
+            target.OnNext("foo");
+            target.OnNext("bar");
+
+            Assert.Equal(new[] { "foo", "foo", "bar" }, result);
+        }
+
+        public static IEnumerable<object[]> GetTargets()
+        {
+            yield return new[] { new Subject<string>() };
+            yield return new[] { new TestSubject() };
+        }
+
+        private class TestSubject : LightweightObservableBase<string>, ISubject<string>
         {
             public void OnNext(string value) => PublishNext(value);
             public void OnCompleted() => PublishCompleted();
