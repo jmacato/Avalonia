2b597204721c438d70aeaecd44a93b731709af80
Jumar Macato <hikari.netto23@gmail.com>
2018-03-23 13:46:25 +0800

    *Create a new project (Avalonia.Animations.Shared) to avoid circular dependencies. *Added Point and Thickness Transitions

--- src/Markup/Avalonia.Markup.Xaml/Converters/EasingTypeConverter.cs
+++ src/Markup/Avalonia.Markup.Xaml/Converters/EasingTypeConverter.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See licence.md file in the project root for full license information.
 
 using Avalonia.Animation;
+using Avalonia.Animation.Easings;
 using System;
 using System.ComponentModel;
 using System.Globalization;
